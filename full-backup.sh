ff2b84fcab24c067dacbfd686e9bdf24d83f470d
Joel Dsouza <joeldsz123@gmail.com>
2023-05-01 08:10:33 +0530

    Update full-backup.sh
    remove drive upload logic. add both media and media to one zip folder for portability

--- full-backup.sh
+++ full-backup.sh
@@ -5,8 +5,7 @@ CONTAINER_NAME="mayan-postgresql-1"
 BACKUP_DIR="/backups"
 DATE=$(date +"%d-%b-%Y")
 DATABASE_NAME="mayan"
-BACKUP_FILENAME_DB="${DATABASE_NAME}_pgdump_${DATE}.sql"
-BACKUP_FILENAME_MEDIA="mayan_media_${DATE}.tar.gz"
+BACKUP_FILENAME="${DATABASE_NAME}_and_media_${DATE}.zip"
 
 # Media Files
 MEDIA_FILES="/var/lib/docker/volumes/mayan_app/_data"
@@ -15,17 +14,14 @@ MEDIA_FILES="/var/lib/docker/volumes/mayan_app/_data"
 mkdir -p ${BACKUP_DIR}
 
 # Run pg_dump command inside the container to create backup file
-docker exec -t ${CONTAINER_NAME} pg_dumpall -c -U mayan > ${BACKUP_DIR}/${BACKUP_FILENAME_DB}
+docker exec -t ${CONTAINER_NAME} pg_dumpall -c -U mayan > ${BACKUP_DIR}/${DATABASE_NAME}_pgdump_${DATE}.sql
 
 # Starting Media Backup.
 echo 'Starting Media Backup'
 cd "$MEDIA_FILES"
 
 # Backup and zip the directory
-zip -r "$BACKUP_DIR/$BACKUP_FILENAME_MEDIA" *
+zip -r "$BACKUP_DIR/mayan_media_${DATE}.zip" .
 
-# Upload backup files to rclone
-rclone copy "$BACKUP_DIR/$BACKUP_FILENAME_DB" drive:/edms-backup/
-rclone copy "$BACKUP_DIR/$BACKUP_FILENAME_MEDIA" drive:/edms-backup/
-
-echo 'Backup files uploaded to rclone successfully'
+# Zip both the database and media backup files into one file
+zip -j "$BACKUP_DIR/$BACKUP_FILENAME" ${BACKUP_DIR}/${DATABASE_NAME}_pgdump_${DATE}.sql "$BACKUP_DIR/mayan_media_${DATE}.zip"
